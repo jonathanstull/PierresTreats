b16687ab52d028a5c313bc77ae78476ca9b67aa0
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-04 12:16:19 -0700

    adds display name to username property

--- PierresTreats/ViewModels/LoginViewModel.cs
+++ PierresTreats/ViewModels/LoginViewModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PierresTreats.ViewModels
 {
   public class LoginViewModel
   {
+    [Display(Name = "Username")]
     public string UserName { get; set; }
     public string Password { get; set; }
   }
