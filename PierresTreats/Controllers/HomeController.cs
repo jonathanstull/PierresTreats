6321bf61295881f080ed6286569b8b3ae2869de0
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-04 14:21:03 -0700

    adds actionlinks for all extant treats and flavors

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,13 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresTreatsContext _db;
+
+    public HomeController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      var allItems = _db.Treats
+          .Include(treat => treat.JoinFTEntities)
+          .ThenInclude(join => join.Flavor);
+      return View(allItems);
     }
   }
 }
