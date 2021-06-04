5e53163f126b662b71ad62c72088d5e789ddd949
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-04 09:11:32 -0700

    completes flavor and treat models, join model

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,15 @@
+namespace PierresTreats.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.JoinFTEntities = new HashSet<FlavorTreat>();
+    }
+    
+    public int FlavorId { get; set; }
+    public string Type { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<FlavorTreat> JoinFTEntities { get; }
+  }
+}
---
+++ PierresTreats/Models/Treat.cs
@@ -0,0 +1,15 @@
+namespace PierresTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinFTEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<FlavorTreat> JoinFTEntities { get; }
+  }
+}
